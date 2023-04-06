c2608812bb4988fda7fafca787d85a918dd43ec3
JopFrenken <87734@roc-teraa.nl>
2023-04-06 11:57:10 +0200

    Update game.js

--- resources/js/api/game.js
+++ resources/js/api/game.js
@@ -4,10 +4,10 @@ export default {
     // sends game data to backend to store in db
     sendGameData: function (name, clicks, time) {
         let obj = {name: name, clicks: clicks, time: time}
-        return axios.post('https://87734.hosting.ict-college.net/Memory-game/api/score', obj)
+        return axios.post('Memory-game/api/score', obj)
     },
 
     getScores: function () {
-        return axios.get('https://87734.hosting.ict-college.net/Memory-game/api/score');
+        return axios.get('Memory-game/api/score');
     }
 }
