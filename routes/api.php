52b23481b5202a17a0672278ff0b3aa4acc94ef9
JopFrenken <87734@roc-teraa.nl>
2023-04-16 16:19:10 +0200

    changed endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
-<?php
+    <?php
 
-use App\Http\Controllers\ScoreController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ScoreController;
+    use App\Http\Controllers\UserController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | is assigned the "api" middleware group. Enjoy building your API!
+    |
+    */
 
-Route::apiResource('score', ScoreController::class);
+    Route::apiResource('score', ScoreController::class);
